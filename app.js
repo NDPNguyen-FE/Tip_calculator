69ad793e0c6657c726ad6cffcc8bd81ad75006c6
NDPNguyen-FE <ndpn.dev@gmail.com>
2021-05-13 10:11:24 +0700

    done

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 function calculateTip() {
-    let billAmt = document.getElementById("billAmount").value;
-    let serviceQual = document.getElementById("options").value;
-    let numOfPeople = document.getElementById("billPeople").value;
-  
-    
-    if (billAmt === "" || serviceQual == 0) {
-      alert("Please enter values");
-      return;
-    }
-    
-    if (numOfPeople === "" || numOfPeople <= 1) {
-      numOfPeople = 1;
-      document.getElementById("each").style.display = "none";
-    } else {
-      document.getElementById("each").style.display = "block";
-    }
-  
-    
-    var total = (billAmt * serviceQual) / numOfPeople;
-    
-    total = Math.round(total * 100) / 100;
-    
-    total = total.toFixed(2);
-    
-    document.getElementById("result").style.display = "block";
-    document.getElementById("resultTip").innerHTML = total;
-  
+  let billAmt = document.getElementById("billAmount").value;
+  let serviceQual = document.getElementById("options").value;
+  let numOfPeople = document.getElementById("billPeople").value;
+
+  if (billAmt === "" || serviceQual == 0) {
+    alert("Please enter values");
+    return;
   }
-  
- 
-  document.getElementById("result").style.display = "none";
-  document.getElementById("each").style.display = "none";
-  
- 
-  document.getElementById("calculate").onclick = function() {
-    calculateTip();
-  
-  };
+
+  if (numOfPeople === "" || numOfPeople <= 1) {
+    numOfPeople = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
+
+  var total = (billAmt * serviceQual) / numOfPeople;
+
+  total = Math.round(total * 100) / 100;
+
+  total = total.toFixed(2);
+
+  document.getElementById("result").style.display = "block";
+  document.getElementById("resultTip").innerHTML = total;
+}
+
+document.getElementById("result").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+document.getElementById("calculate").onclick = function () {
+  calculateTip();
+};
